cff2ffb1b309d4d7548f87fd85ba6cd8541aa4fb
luxorylife <lukash.201@mail.ru>
2022-03-13 21:08:52 +0800

    docker postgres + conencting to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module";
+
+require("dotenv").config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
+require("dotenv").config();
+
+async function start() {
+  const PORT = process.env.PORT || 3800;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(PORT, () => console.log(`Server starts on port ${PORT}`));
 }
-bootstrap();
+
+start();
